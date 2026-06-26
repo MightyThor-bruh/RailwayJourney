4f1adc63b8d022d017fe112bb616f8163ef95a38
Lisa Shumova <73255984+MightyThor-bruh@users.noreply.github.com>
2026-06-26 15:57:00 +0300

    fixed bug

--- Assets/Scripts/CamScript.cs
+++ Assets/Scripts/CamScript.cs
@@ -8,10 +8,10 @@ public class CamScript : MonoBehaviour
     float scrollSpeed = 10f;
     [SerializeField]
     int sensivity = 1;
-    [SerializeField]
-    int maxdistance = 33;
-    [SerializeField]
-    int mindistance = -150;
+   // [SerializeField]
+   // int maxdistance = 33;
+   // [SerializeField]
+   // int mindistance = -150;
 
     [SerializeField]
     public Transform targetPos;
@@ -25,8 +25,6 @@ public class CamScript : MonoBehaviour
         if (x != 0 || y != 0)
         {
             Vector3 newpos = transform.position + (transform.TransformDirection(new Vector3(x, 0, 0)) + Vector3.up * y) / sensivity;
-            //if (ControlDistance(Vector3.Distance(newpos, targetPos.position)))
-            //    transform.position = newpos;
             transform.position = newpos;
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -26, 29),
                              Mathf.Clamp(transform.position.y, 3, 33),
@@ -36,8 +34,6 @@ public class CamScript : MonoBehaviour
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
             Vector3 newpos = transform.position + transform.TransformDirection(Vector3.forward * Input.GetAxis("Mouse ScrollWheel") * scrollSpeed);
-            //if (ControlDistance(Vector3.Distance(newpos, targetPos.position)))
-            //    transform.position = newpos;
             transform.position = newpos;
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -26, 29),
                                   Mathf.Clamp(transform.position.y, 3, 33),
@@ -49,22 +45,21 @@ public class CamScript : MonoBehaviour
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, -26, 35),
                       Mathf.Clamp(transform.position.y, 3, 33),
                       Mathf.Clamp(transform.position.z, -125, -58));
-            transform.RotateAround(targetPos.position, Vector3.up, Input.GetAxis("Mouse X") * sensivity);
-            //transform.RotateAround(targetPos.position, Vector3.up, Input.GetAxis("Mouse X") * sensivity);           
+            transform.RotateAround(targetPos.position, Vector3.up, Input.GetAxis("Mouse X") * sensivity);     
         }
     }
 
-    bool ControlDistance(float distance)
-    {
-        if (distance > mindistance && distance < maxdistance)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+   // bool ControlDistance(float distance)
+  //  {
+  //      if (distance > mindistance && distance < maxdistance)
+   //     {
+   //         return true;
+   //     }
+   //     else
+   //     {
+   //         return false;
+   //     }
+   // }
 
     bool move = false;
     Vector3 startPosition;
@@ -81,7 +76,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-8f, 17f, -87f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
 
@@ -94,7 +89,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(10f, 17f, -86f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
 
@@ -108,7 +103,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(2.95f, 20f, -110.28f);
             needRotation = Quaternion.Euler(6.977f, 0f, 0f);
 
@@ -122,7 +117,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(1f, 15f, -88f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -135,7 +130,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(0.25f, 8f, -88f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -148,7 +143,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-12.5f, 13f, -88.5f);
             needRotation = Quaternion.Euler(0f, -12f, 0f);
         }
@@ -161,7 +156,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-16.59f, 20f, -81.93f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -174,7 +169,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-6.61f, 24.52f, -80.28f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -187,7 +182,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(6f, 25f, -88f);
             needRotation = Quaternion.Euler(0f, 70f, 0f);
         }
@@ -200,7 +195,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-2.34f, 23.92f, -78.25f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -213,7 +208,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(13f, 18.56f, -87f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -226,7 +221,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-13f, 18.73f, -87f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -239,7 +234,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-10.54f, 11f, -92.56f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -252,7 +247,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-10.54f, 11f, -92.56f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
@@ -265,7 +260,7 @@ public class CamScript : MonoBehaviour
         {
             move = true;
             startPosition = transform.position;
-            needRotation = transform.rotation;
+            startRotation = transform.rotation;
             needPosition = new Vector3(-0.26f, 21.59f, -84.56f);
             needRotation = Quaternion.Euler(0f, 0f, 0f);
         }
